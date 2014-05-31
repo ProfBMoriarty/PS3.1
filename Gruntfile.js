2f9eff6da829d83db0251397f62fb8cf7ef4b789
Mark Diehr <mdiehr@gmail.com>
2014-05-30 23:52:24 -0400

    Example task for concat Task to build minified ps/aq files in a build folder (which isn't under revision control) Renamed as-min and aq-min Clean task to remove tmp folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,43 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+ // Project configuration.
+ grunt.initConfig({
+   ps: grunt.file.readJSON('package.json'),
+   aq: grunt.file.readJSON('packageAQ.json'),
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/<%= ps.file %>.js'],
+        dest: 'tmp/<%= ps.file %>.js'
+      }
+    },
+
     uglify: {
       options: {
-        banner: '/*! <%= pkg.longname %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= ps.name %> <%= ps.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+        files: {
+          'build/<%= ps.file %>.min.js': ['tmp/<%= ps.file %>.js'],
+          'build/<%= aq.file %>.min.js': ['src/<%= aq.file %>.js']
+        }
       }
+    },
+
+    clean: {
+      build: ["tmp"]
     }
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'clean']);
 
 };
