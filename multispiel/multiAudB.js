86662b3c0451c287e34a002c016cc7720a232f60
Mark Diehr <mdiehr@gmail.com>
2014-12-16 22:53:45 -0500

    Audio Testing Framework for Multispiel

---
+++ multispiel/multiAudB.js
@@ -0,0 +1,49 @@
+// Test
+
+(function() {
+
+var myName = "multiAudB";
+var PS = PERLENSPIEL.Create({namespace:myName});
+
+// Put your global variables after this line
+var sounds = ["fx_zurp", "fx_shoot2", "fx_rip"];
+var color = 0x99FF99;
+
+// Put your function definitions after this line
+function DrawText(text, x, y) {
+	for (var i = 0; i < text.length; ++i) {
+		PS.glyph(x+i, y, text[i]);
+	}
+}
+
+// PS.init( system, options )
+// Initializes the game
+PS.init = function (system, options) {
+	"use strict";
+
+	PS.gridSize(9, sounds.length);
+
+	PS.statusText(myName);
+	PS.color(PS.ALL, PS.ALL, color);
+	for (var s = 0; s < sounds.length; ++s) {
+		PS.audioLoad(sounds[s]);
+		DrawText(sounds[s], 0, s);
+	}
+};
+
+PS.touch = function (x, y, data, options) {
+	"use strict";
+	PS.audioPlay(sounds[y]);
+};
+
+PS.release = function (x, y, data, options) { "use strict"; };
+PS.enter = function (x, y, data, options) { "use strict"; };
+PS.exit = function (x, y, data, options) { "use strict"; };
+PS.exitGrid = function (options) { "use strict"; };
+PS.keyDown = function (key, shift, ctrl, options) { "use strict"; };
+PS.keyUp = function (key, shift, ctrl, options) { "use strict"; };
+PS.input = function (sensors, options) { "use strict"; };
+
+// Start the engine!
+PS.start();
+})();
